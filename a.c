945b35fa5c9f9be547b4ee49ef87db41387f9293
ahmedmadbouly186 <ahmedmadbouly186@gmail.com>
2022-10-13 12:32:14 +0200

    changing the sum to multiplye

--- a.c
+++ a.c
@@ -3,5 +3,5 @@ void main(){
 	printf("hello \nplease enter two numbers: ");
 	long a,b;
 	scanf("%d %d",&a,&b);
-	printf("the sum of this two numbers is: %d",a+b);
+	printf("the multiplie of this two numbers is: %d",a*b);
 }
