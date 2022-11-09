793c614d96c9d88f523ef1cc976a94ff69fd0839
Amr Ahmed <amroahmed50@outlook.com>
2022-11-09 23:35:09 +0200

    added .env

--- a.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main(){
-	//ahmed branch
-	printf("this is an update\n");
-	printf("hello \nplease enter two numbers: ");
-	long a,b;
-	scanf("%d %d",&a,&b);
-	printf("the divided of this two numbers is: %d",a/b);
-}
