f36b198d2db5d960cbd4f4efc4c3a4f471cb24b2
ahmedmadbouly186 <ahmedmadbouly186@gmail.com>
2022-11-11 15:56:52 +0200

    change the arch

--- lib/shared/component/component.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-void navigateTo(context,Widget)=>Navigator.push(context,
-    MaterialPageRoute(builder: (context)=>Widget)
-);
