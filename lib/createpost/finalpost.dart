ef81951a447765a203772d5d9acff89a6c675c96
Ahmed Fawzy <99263226+belfooz@users.noreply.github.com>
2022-11-13 01:29:00 +0200

    Add files via upload

--- lib/createpost/finalpost.dart
+++ lib/createpost/finalpost.dart
@@ -1,22 +1,26 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:reddit/modules/createpost/schedulepost.dart';
-import 'package:reddit/shared/style/icon_broken.dart';
+import 'package:post/home/screens/home_layout.dart';
+import '../../icons/icon_broken.dart';
+import './schedulepost.dart';
 
 class finalPostScreen extends StatefulWidget {
-  const finalPostScreen({Key? key}) : super(key: key);
 
+  const finalPostScreen({Key? key}) : super(key: key);
   @override
   State<finalPostScreen> createState() => _finalPostScreenState();
 }
+var colorOfSpoiler= Colors.grey[100];
+var colorOfNSFW= Colors.grey[100];
 
 class _finalPostScreenState extends State<finalPostScreen> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-
       body: Column(
         children: [
           SizedBox(height: 40.0),
@@ -24,22 +28,31 @@ class _finalPostScreenState extends State<finalPostScreen> {
             children: [
               Padding(
                 padding: const EdgeInsetsDirectional.only(start: 10.0),
-                child: Icon(IconBroken.Arrow___Left_2,size: 32.0, color: Colors.black,),
+                child: Icon(
+                  IconBroken.Arrow___Left_2,
+                  size: 32.0,
+                  color: Colors.black,
+                ),
               ),
-              SizedBox(width: 250.0,),
+              SizedBox(
+                width: 250.0,
+              ),
               Padding(
                 padding: const EdgeInsetsDirectional.only(end: 30.0),
-                child: MaterialButton(onPressed: (){
-                },
-
+                child: MaterialButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                    MaterialPageRoute(builder: (context)=>homeLayoutScreen())
+                    );
+                  },
                   elevation: 0.0,
                   height: 40.0,
                   minWidth: 80.0,
                   shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0)
-                  ),
+                      borderRadius: BorderRadius.circular(20.0)),
                   color: Colors.blue[900],
-                  child: Text("post",
+                  child: Text(
+                    "post",
                     style: TextStyle(
                       color: Colors.white,
                     ),
@@ -48,7 +61,9 @@ class _finalPostScreenState extends State<finalPostScreen> {
               )
             ],
           ),
-          SizedBox(height: 25.0,),
+          SizedBox(
+            height: 25.0,
+          ),
           Padding(
             padding: const EdgeInsetsDirectional.only(start: 20.0),
             child: Row(
@@ -59,38 +74,63 @@ class _finalPostScreenState extends State<finalPostScreen> {
                       backgroundColor: Colors.blue,
                       radius: 20.0,
                     ),
-                    SizedBox(width: 8.0,),
-                    ElevatedButton.icon(onPressed: (){}, icon: Text("ascdavwVWRV",
-                      style: TextStyle(
-                          color: Colors.black
-                      ),
+                    SizedBox(
+                      width: 8.0,
                     ),
-                      label: Icon(IconBroken.Arrow___Down_2,color: Colors.black,size: 15,),
-                      style:ButtonStyle(
+                    ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Text(
+                        "r/Cross-platform",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      label: Icon(
+                        IconBroken.Arrow___Down_2,
+                        color: Colors.black,
+                        size: 15,
+                      ),
+                      style: ButtonStyle(
                         elevation: MaterialStateProperty.all<double>(0),
                         //fixedSize: MaterialStateProperty.all(Size(100.0, 20.0)),
-                        backgroundColor:MaterialStateProperty.all(Colors.white),
-                      ) ,
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.white),
+                      ),
                     ),
-
                   ],
                 ),
                 Spacer(),
-                TextButton(onPressed: ()=>showModalBottomSheet(context: context, builder: (context)=>
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(child: Center(child: Text("Community Standards"))),
-                      ElevatedButton(onPressed: (){
-                        Navigator.pop(context);
-                      }, child: Text("understand"))
-                    ],
-                  ),
-                )
-                ),
-                    child: Text("Rules")
-                )
+                TextButton(
+                    onPressed: () => showModalBottomSheet(
+                        context: context,
+                        builder: (context) => Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Expanded(
+                                      child: Center(
+                                          child: Text("Community Standards"))),
+                                  ElevatedButton(
+                                   style: ButtonStyle(
+                                     backgroundColor: MaterialStateProperty.all(
+                                       Colors.blue[900],
+                                     ),
+                                   ),
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text("understand",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                      ),
+                                  )
+                                ],
+                              ),
+                            )),
+                    child: Text("Rules",
+                    style: TextStyle(
+                      color: Colors.blue
+                    ),
+                    ))
               ],
             ),
           ),
@@ -101,6 +141,7 @@ class _finalPostScreenState extends State<finalPostScreen> {
               style: TextStyle(
                   fontSize: 20.0
               ),
+              maxLines: 2,
               decoration: InputDecoration(
                   contentPadding:const EdgeInsets.all(10.0),
                   border: OutlineInputBorder(
@@ -111,53 +152,96 @@ class _finalPostScreenState extends State<finalPostScreen> {
           ),
           Row(
             children: [
-              SizedBox(width: 20.0,),
-              ElevatedButton.icon(onPressed: (){},
-                  style:ButtonStyle(
+              SizedBox(
+                width: 20.0,
+              ),
+              ElevatedButton.icon(
+                  onPressed: () {
+                    setState(() {
+                      if(colorOfNSFW==Colors.grey[100])
+                      {
+                        colorOfNSFW=Colors.black87;
+                      }
+                      else if(colorOfNSFW==Colors.black87)
+                      {
+                        colorOfNSFW=Colors.grey[100];
+                      }
+                    });
+                  },
+                  style: ButtonStyle(
                     elevation: MaterialStateProperty.all<double>(0),
                     fixedSize: MaterialStateProperty.all(Size(130.0, 20.0)),
-                    backgroundColor:MaterialStateProperty.all( Colors.grey[100],),
-                  ) ,
-                  label: Text("NSFW",
-                    style: TextStyle(
-                      color: Colors.grey[600],
+                    backgroundColor: MaterialStateProperty.all(
+                      colorOfNSFW,
                     ),
                   ),
-                  icon:Text("18",
-                    style:TextStyle(color: Colors.grey[600],)
-                    ,)
-
+                  label: Text(
+                    "NSFW",
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                  icon: Text(
+                    "18",
+                    style: TextStyle(
+                      color:Colors.grey[400],
+                    ),
+                  )),
+              SizedBox(
+                width: 10.0,
               ),
-              SizedBox(width: 10.0,),
-              ElevatedButton.icon(onPressed: (){},
-                style:ButtonStyle(
+              ElevatedButton.icon(
+                onPressed: () {
+                  setState(() {
+                    if(colorOfSpoiler==Colors.grey[100])
+                      {
+                        colorOfSpoiler=Colors.black87;
+                      }
+                    else if(colorOfSpoiler==Colors.black87)
+                      {
+                        colorOfSpoiler=Colors.grey[100];
+                      }
+                  });
+                },
+                style: ButtonStyle(
                   elevation: MaterialStateProperty.all<double>(0),
                   fixedSize: MaterialStateProperty.all(Size(122.0, 20.0)),
-                  backgroundColor:MaterialStateProperty.all( Colors.grey[100],),
-                ) ,
-                label: Text("Spoiler",
-                  style: TextStyle(
-                    color: Colors.grey[600],
+                  backgroundColor: MaterialStateProperty.all(
+                   colorOfSpoiler,
                   ),
                 ),
-                icon: Icon(IconBroken.Danger,color: Colors.grey[600],),
-
+                label: Text(
+                  "Spoiler",
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                  ),
+                ),
+                icon: Icon(
+                  IconBroken.Danger,
+                  color: Colors.grey[400],
+                ),
               ),
             ],
           ),
-          SizedBox(height: 8.0,),
+          SizedBox(
+            height: 8.0,
+          ),
           ListTile(
             horizontalTitleGap: 0.0,
             title: Text("Add flair"),
             leading: Icon(IconBroken.Edit),
             trailing: Icon(IconBroken.Arrow___Right_2),
           ),
-          Divider(height: 10.0,color: Colors.grey,),
+          Divider(
+            height: 10.0,
+            color: Colors.grey,
+          ),
           ListTile(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>
-              schedulePostScreen()
-              ));
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => schedulePostScreen()));
             },
             horizontalTitleGap: 0.0,
             title: Text("Schedule Post"),
--- lib/createpost/schedulepost.dart
+++ lib/createpost/schedulepost.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:lite_rolling_switch/lite_rolling_switch.dart';
 
 class schedulePostScreen extends StatefulWidget {
   const schedulePostScreen({Key? key}) : super(key: key);
@@ -14,53 +13,48 @@ class _schedulePostScreenState extends State<schedulePostScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
         backgroundColor: Colors.white,
-        elevation: 2.0,
-        systemOverlayStyle: SystemUiOverlayStyle(
-          statusBarColor: Colors.white,
-          statusBarIconBrightness: Brightness.dark
-        ),
-        leading: IconButton(
-          icon: Icon(Icons.close,size: 30.0,color: Colors.black,),
-          onPressed: ()
-          {
-            Navigator.pop(context);
-          },
-        ),
-        title: Text(
-          "Schedule Post",
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 2.0,
+          systemOverlayStyle: SystemUiOverlayStyle(
+              statusBarColor: Colors.white,
+              statusBarIconBrightness: Brightness.dark),
+          leading: IconButton(
+            icon: Icon(
+              Icons.close,
+              size: 30.0,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
           ),
+          title: Text(
+            "Schedule Post",
+            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          ),
+          actions: [TextButton(onPressed: () {}, child: Text("Save"))],
         ),
-        actions: [
-          TextButton(onPressed: (){}, child: 
-          Text("Save")
-          )
-        ],
-      ),
-      body: Column(
-        children: [
-          SizedBox(height: 7.0,),
+        body: Column(children: [
+          SizedBox(
+            height: 7.0,
+          ),
           Padding(
             padding: const EdgeInsetsDirectional.only(start: 15.0),
             child: Row(
-
-              children: [
-                Text("Starts")
-              ],
+              children: [Text("Starts")],
             ),
           ),
-          SizedBox(height: 10.0,),
+          SizedBox(
+            height: 10.0,
+          ),
           Padding(
             padding: const EdgeInsetsDirectional.only(start: 15.0),
             child: Row(
               children: [
                 Expanded(child: Text("Repeat weekly on Friday")),
-                Padding(
+                /*Padding(
                   padding: const EdgeInsetsDirectional.only(end: 15.0),
                   child: Container(
                     width: 100,
@@ -76,12 +70,10 @@ class _schedulePostScreenState extends State<schedulePostScreen> {
                       onTap:  (){},
                     ),
                   ),
-                )
+                )*/
               ],
             ),
           )
-         ]
-      )
-    );
+        ]));
   }
 }
