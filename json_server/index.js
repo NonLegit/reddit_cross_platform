dc65b6306b724139f96e479e64dd6ef3997ad217
Amr Ahmed <amroahmed50@outlook.com>
2022-11-24 03:05:29 +0200

    additional changes to json server

--- json_server/index.js
+++ json_server/index.js
@@ -10,6 +10,21 @@ server.use(jsonServer.bodyParser);
 server.use(middlewares);
 
 // Add custom routes before JSON Server router
+server.get('/subreddits/mine/where', (req, res) => {
+	res.jsonp({
+		status: 'success',
+		subreddits: [
+			{
+				icon: 'Icon of subreddit',
+				id: '10',
+				subredditName: 'r/any_name',
+				membersCount: 10000,
+				description: 'Wellcome to my subreddit',
+			},
+		],
+	});
+});
+
 server.get('/users/me/', (req, res) => {
 	res.redirect('/users?userName=Zeinab_maoawad');
 });
@@ -73,10 +88,47 @@ server.post('/users/login/', (req, res) => {
 
 // server.use((req, res, next) => {
 // 	if (req.method === 'POST') {
-// 		// if (req.body.name != null) {
-// 		// 	req.body.id = req.body.name;
-// 		// 	res.body = 'hi';
-// 		// }
+// 		if (req.originalUrl.includes('/subreddits')) {
+// 			req.body.displayName = 'test';
+// 			req.body.icon =
+// 				'https://previews.123rf.com/images/seamartini/seamartini1609/seamartini160900764/64950290-chef-toque-vector-sketch-icon-cook-cap-kitchen-cooking-hat-emblem-for-restaurant-design-element-bake.jpg';
+// 			req.body.backgroundImage =
+// 				'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fassets.marthastewart.com%2Fstyles%2Fwmax-750%2Fd30%2Feasy-basic-pancakes-horiz-1022%2Feasy-basic-pancakes-horiz-1022_0.jpg%3Fitok%3DXQMZkp_j';
+// 			req.body.description = 'test description';
+// 			req.body.subredditLink =
+// 				'https://previews.123rf.com/images/seamartini/seamartini1609/seamartini160900764/64950290-chef-toque-vector-sketch-icon-cook-cap-kitchen-cooking-hat-emblem-for-restaurant-design-element-bake.jpg';
+// 			req.body.numOfMembers = 12345;
+// 			req.body.numOfOnlines = 321;
+// 			req.body.rules = [
+// 				{
+// 					title: 'no codeing',
+// 					description: 'i hate coding',
+// 				},
+// 				{
+// 					title: 'no codeing',
+// 					description: 'i hate coding',
+// 				},
+// 				{
+// 					title: 'no codeing',
+// 					description: 'i hate coding',
+// 				},
+// 			];
+// 			req.body.moderators = [
+// 				{
+// 					userName: 'Ali',
+// 				},
+// 				{
+// 					userName: 'omer',
+// 				},
+// 				{
+// 					userName: 'zeinab',
+// 				},
+// 				{
+// 					userName: 'mazen',
+// 				},
+// 			];
+// 			req.isJoined = true;
+// 		}
 // 	}
 // 	// Continue to JSON Server router
 // 	next();
@@ -97,20 +149,12 @@ router.render = (req, res) => {
 				} else {
 					return defaultGET(req, res);
 				}
-
-				// return res.locals.data.length > 1
-				// 	? res.jsonp({
-				// 			status: 'success',
-				// 			data: res.locals.data,
-				// 	  })
-				// 	: res.jsonp({
-				// 			status: 'success',
-				// 			data: res.locals.data[0],
-				// 	  });
 			} else if (req.method === 'POST') {
 				if (req.originalUrl.includes('/subreddits')) {
 					return subredditPOST(req, res);
-				}
+				} else if (req.originalUrl.includes('/post')) {
+					return postPOST(req, res);
+				} else return res.locals.data;
 			} else return res.locals.data;
 		})()
 	);
@@ -133,5 +177,9 @@ const subredditGET = (req, res) => {
 };
 
 const subredditPOST = (req, res) => {
-	return res.jsonp({ id: res.locals.data.id });
+	return res.jsonp({ status: 'success', id: res.locals.data.id });
+};
+
+const postPOST = (req, res) => {
+	return res.jsonp({ status: 'success', id: res.locals.data.id });
 };
