8c079867209397876f90ffb15b5fab90493c19d8
emanshahda <eman.shahda01@eng-st.cu.edu.eg>
2022-12-23 11:57:31 +0200

    Solve Error

--- lib/messages/screens/show_message_body.dart
+++ lib/messages/screens/show_message_body.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:post/messages/provider/message_provider.dart';
+import '../../messages/provider/message_provider.dart';
 import 'package:provider/provider.dart';
 
 import '../models/user_message.dart';
